a5fe4d8bcc7f5e9443d2c5275da08697eef10ddb
Alessandro-Al <alfieri_alessand@hotmail.com>
2026-06-02 14:48:54 -0300

    nova versao

--- LK300/fw.js
+++ LK300/fw.js
@@ -1,27 +1,27 @@
 const FIRMWARES = {
   vp1200: {
     title: "VP12[xx]",
-    versao: "V5.12",
+    versao: "V5.13",
     subtitle: "Verificadora de Peso"
   },
   cl3500: {
     title: "CL3500",
-    versao: "V5.12",
+    versao: "V5.13",
     subtitle: "Classificadora de Piso"
   },
   lk305: {
     title: "LK305",
-    versao: "V5.12",
+    versao: "V5.13",
     subtitle: "Balança Estática com Dosagem"
   },
   sorter: {
     title: "SORTER",
-    versao: "V5.12",
+    versao: "V5.13",
     subtitle: "Separador de Caixas"
   },
   ap300: {
     title: "AP3[xx]",
-    versao: "V5.12",
+    versao: "V5.13",
     subtitle: "Impressora com Aplicador"
   },
   mc5000: {
