8c2adc05d9375c87a2dc0a07e49bad59abaf1acd
Alessandro-Al <alfieri_alessand@hotmail.com>
2026-07-21 19:41:21 -0300

    nova versao

--- LK200/fw.js
+++ LK200/fw.js
@@ -1,7 +1,7 @@
 const FIRMWARES = {
     vp1200: {
         title: "VP12[xx]",
-        versao: "V9.16_nck",
+        versao: "V9.17_nck",
         subtitle: "Verificadora de Peso"
     },
     vp9000: {
